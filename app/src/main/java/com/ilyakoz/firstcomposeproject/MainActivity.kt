6a76846820557c6318c07aca15dfe92e08dc637b
Ilya Kozirskiy <ilyadevkoz@gmail.com>
2023-07-18 12:33:59 +0300

    image

--- app/src/main/java/com/ilyakoz/firstcomposeproject/MainActivity.kt
+++ app/src/main/java/com/ilyakoz/firstcomposeproject/MainActivity.kt
@@ -5,15 +5,27 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ilyakoz.firstcomposeproject.ui.theme.FirstComposeProjectTheme
 import com.ilyakoz.firstcomposeproject.ui.theme.InstagramProfileCard
 
@@ -21,7 +33,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeProjectTheme{
+            FirstComposeProjectTheme {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
@@ -33,6 +45,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ImageText() {
+    Image(
+        modifier = Modifier.clip(CircleShape),
+        painter = painterResource(id = R.drawable.ic_instagram),
+        contentDescription = null,
+        contentScale = ContentScale.Inside
+
+    )
 }
 
 
--- app/src/main/java/com/ilyakoz/firstcomposeproject/ui/theme/InstagramProfileCard.kt
+++ app/src/main/java/com/ilyakoz/firstcomposeproject/ui/theme/InstagramProfileCard.kt
@@ -1,18 +1,28 @@
 package com.ilyakoz.firstcomposeproject.ui.theme
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ilyakoz.firstcomposeproject.R
 
 @Composable
 fun InstagramProfileCard() {
@@ -31,14 +41,14 @@ fun InstagramProfileCard() {
 
 
             ) {
-            Box(
-                modifier = Modifier
-                    .size(80.dp)
-                    .background(Color.Green)
+            Image(
+                modifier = Modifier.clip(CircleShape),
+                contentDescription = null,
+                painter = painterResource(id = R.drawable.ic_instagram),
             )
-            TwoBox()
-            TwoBox()
-            TwoBox()
+            UserStatistics(title = "Posts", value = "6,970")
+            UserStatistics(title = "Followers", value = "436M")
+            UserStatistics(title = "Following", value = "76")
 
 
         }
@@ -47,31 +57,33 @@ fun InstagramProfileCard() {
 }
 
 @Composable
-private fun TwoBox() {
-    Box(
+private fun UserStatistics(
+    title: String,
+    value: String
+) {
+
+    Column(
         modifier = Modifier
-            .size(80.dp)
-            .background(Color.Yellow)
+            .height(80.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly,
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxHeight()
-        ) {
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .background(Color.Black)
-                    .fillMaxWidth()
-            )
-            Box(
-                modifier = Modifier
-                    .weight(1f)
-                    .background(Color.Yellow)
-                    .fillMaxWidth()
-            )
-        }
+        Text(
+            text = value,
+            fontFamily = FontFamily.Cursive,
+            fontStyle = FontStyle.Italic,
+            fontSize = 24.sp
+        )
+        Text(
+            text = title,
+            fontFamily = FontFamily.Serif,
+            fontStyle = FontStyle.Italic,
+            fontWeight = FontWeight.Bold
+
+        )
     }
 }
+
 
 @Preview
 @Composable
